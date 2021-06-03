b1f4bfbab02abfb2fcd9e7c1cc4788537a5f2f28
ddony8128 <62324495+ddony8128@users.noreply.github.com>
2021-06-03 15:54:50 +0900

    Add files via upload
    for performance test

---
+++ include/performance_test.h
@@ -0,0 +1,48 @@
+/**
+ * @file performance_test.h
+ *
+ */
+
+#ifndef PERFORMANCE_TEST_
+#define PERFORMANCE_TEST_
+
+#include <stdio.h>
+#include <time.h>
+#include "common.h"
+#include "graph.h"
+#include "candidate_set.h"
+
+
+class PerformanceTest{
+    public: 
+        PerformanceTest(){};  
+        ~PerformanceTest(){};
+
+        void start(){
+            this->startT = time(NULL);
+        }
+
+        void end(){
+            this->endT = time(NULL);
+        }
+
+        void check_result(const Graph &data, const Graph &query,
+                                const CandidateSet &cs, const std::string &result_file );
+
+        void print_result();
+
+    private:
+    
+        time_t startT;
+        time_t endT;
+        int answer_num;
+        int correct_num;
+    
+
+        bool check(const Graph &data, const Graph &query,
+                                const CandidateSet &cs, int *answer, size_t len_query);
+};
+
+
+
+#endif // PERFORMANCE_TEST_
