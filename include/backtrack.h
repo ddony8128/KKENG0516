bfa2722566a1afd1cec96ac132d70833114574a3
ddony8128 <62324495+ddony8128@users.noreply.github.com>
2021-06-07 16:07:46 +0900

    Update backtrack.h
    just update

--- include/backtrack.h
+++ include/backtrack.h
@@ -9,14 +9,36 @@
 #include "candidate_set.h"
 #include "common.h"
 #include "graph.h"
+#include <stack>
+
+using namespace std;
 
 class Backtrack {
- public:
-  Backtrack();
-  ~Backtrack();
+    public:
+        Backtrack();
+        ~Backtrack();
 
-  void PrintAllMatches(const Graph &data, const Graph &query,
+        void PrintAllMatches(const Graph &data, const Graph &query,
                        const CandidateSet &cs);
+    private:
+
+
+        Vertex RootForDAG(const Graph &query, const CandidateSet &cs);
+
+        void BuildDAG(const Graph &query, const CandidateSet &cs, Vertex root, vector<vector<Vertex>> &DAG);
+
+        void BuildReverseDAG(vector<vector<Vertex>> &DAG, vector<vector<Vertex>> &RDAG);
+
+        bool backtrack_1(const Graph &data, const Graph &query,
+                                const CandidateSet &cs, vector<Vertex> &illuminated_cs, Vertex k);
+
+        bool backtrack_2(const Graph &data, const Graph &query,
+                                const CandidateSet &cs, vector<Vertex> &illuminated_cs, Vertex k);
+
+        bool backtrack_3();
+
+        void inactivate(Vertex denied);
+
 };
 
 #endif  // BACKTRACK_H_
